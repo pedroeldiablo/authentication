d80736217da40969b0beca880ce6359d85c6ab8d
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-05 16:49:09 +0100

    adds babel to allow es6 syntax

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+const app = express();
+
+//App Setup
+
+
+//Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on ', port);
