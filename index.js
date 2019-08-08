7407653ca666e5b6c43af562d44e3cf7c47cb80f
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-08 15:38:18 +0100

    allows cors on whitelists domains

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import http from 'http';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 
@@ -12,14 +13,27 @@ mongoose.connect('mongodb://localhost:auth/auth', {
     useNewUrlParser: true,
 });
 
+
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 // App Setup
 app.use(morgan('combined'));
+app.use(cors(corsOptions));
 app.use(
     bodyParser.json({
         type: '*/*',
     }),
 );
 router(app);
+
 
 // Server Setup
 const port = process.env.PORT || 3090;
